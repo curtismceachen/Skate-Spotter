c37db851ab1a7bed66488ad26894c54d20765f90
user <user@users-MacBook-Pro.local>
2022-06-10 19:44:36 -0400

    connected to database

---
+++ controllers/testing.js
@@ -0,0 +1,9 @@
+const testing = require('../src/App');
+
+module.exports = {
+    console
+}
+
+function console() {
+    console.log("IT WORKS!")
+}
--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <button onClick={console}>Click Me!</button> */}
       </header>
     </div>
   );
